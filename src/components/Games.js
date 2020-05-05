8988995796a7b6247679a809650f9a0221d71bc2
wshilman <walter.shilman@bue.edu.ar>
2020-05-05 20:27:57 -0300

    fix visualizacion de cartas

--- src/components/Games.js
+++ src/components/Games.js
@@ -2,42 +2,47 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { Grid } from '@material-ui/core';
 import Nav from './Nav'
-
 import suma from '../media/suma.png'
 import resta from '../media/resta.png'
 import division from '../media/division.png'
 
 const useStyles = makeStyles({
-  root: {
+  card: {
     maxWidth: 200,
-    marginLeft:20,
-    marginRight:20
-    // padding: 112
+    marginLeft:"14%",
+    alignItems:"center",
+    marginBottom: 15
   },
   media: {
     height: 140,
   },
   grid:{
     display:"flex",
-    alignItems:"center"
+    alignItems:"center",
+    height:"100%",
+    width:"100%"
+  },
+  container:{
+    marginTop:"12%",
+    alignItems:"center",
   }
 });
 
-export default function MediaCard() {
+const Games = props =>{
+  let { nombre, apellido } = props
+
   const classes = useStyles();
 
   return (
-    <div>
-      {/* <Nav name = "walter"/>  TODO*/}
+    <div className={classes.container}>
+      <Nav name = {nombre}/>
       <Grid container className= {classes.grid}>
-        <Card className={classes.root}>
+        <Card className={classes.card}>
           <CardActionArea>
             <CardMedia
               className={classes.media}
@@ -49,13 +54,12 @@ export default function MediaCard() {
                 Juego 1
               </Typography>
               <Typography variant="body2" color="textSecondary" component="p">
-                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                across all continents except Antarctica
+                Esta es la descripcion del juego 1
               </Typography>
             </CardContent>
           </CardActionArea>
         </Card>
-        <Card className={classes.root}>
+        <Card className={classes.card}>
           <CardActionArea>
               <CardMedia
                 className={classes.media}
@@ -67,13 +71,12 @@ export default function MediaCard() {
                   Juego 2
                 </Typography>
                 <Typography variant="body2" color="textSecondary" component="p">
-                  Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                  across all continents except Antarctica
+                  Esta es la descripcion del juego 2
                 </Typography>
               </CardContent>
             </CardActionArea>
           </Card>
-          <Card className={classes.root}>
+          <Card className={classes.card}>
           <CardActionArea>
               <CardMedia
                 className={classes.media}
@@ -85,16 +88,18 @@ export default function MediaCard() {
                   Juego 3
                 </Typography>
                 <Typography variant="body2" color="textSecondary" component="p">
-                  Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                  across all continents except Antarctica
+                  Esta es la descripcion del juego 3
                 </Typography>
               </CardContent>
             </CardActionArea>
           </Card>
-          
+
 
 
       </Grid>
     </div>
   );
 }
+
+
+export default Games;
