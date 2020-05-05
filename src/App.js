468a6d3059bcea0c22cb003e23304aa3d1ee9e01
wshilman <walter.shilman@bue.edu.ar>
2020-05-05 20:27:45 -0300

    agrego nav y cambio routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Login from './components/Login'
 import Games from './components/Games'
+import Nav from './components//Nav'
+
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
+const App = ()=> {
+  const [nombre, setNombre] = useState("Walter");
+  const [apellido, setApellido] = useState("");
+
   return (
+
+    
     <Router>
       <div className="App">
+        <Nav nombre={nombre}/>
         <Switch>
-        <Route path="/" exact component={Login}/>
-        <Route path="/juegos" exact component={Games}/>
+          <Route path="/" exact component={() => <Login nombre={nombre} setNombre={setNombre}
+          apellido={apellido} setApellido={setApellido}/>} />
+
+          <Route path="/juegos" exact component={Games}/>
 
         </Switch>
       </div>
