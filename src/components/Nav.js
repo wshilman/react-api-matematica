0ce11842725409252843e4f60889581339134403
wshilman <walter.shilman@bue.edu.ar>
2020-05-05 22:25:43 -0300

    nav styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,32 @@
 import React, {useState} from "react";
 
 import { makeStyles } from '@material-ui/core/styles';
-import { grey } from '@material-ui/core/colors';
-import { Component } from 'react';
+
+const useStyles = makeStyles({
+    text:{
+        fontSize: 25,
+        textAlign:"left",
+        marginLeft: 5,
+
+
+    },
+    nav:{
+        height: 35,
+        backgroundColor:"#1876D2",
+        marginTop:-18,
+        marginBottom:0,
+    }
+});
 
 const Nav = ({nombre})=>{
+    const classes = useStyles();
+
     return(
         <div>
             {
-        nombre && 
-        <div style={
-            {height: 35,
-            backgroundColor:"#1876D2",
-            marginTop:-20,
-            marginBottom:0}
-        }>
-                <h1>Iniciado como: {nombre}</h1>
+        nombre &&
+        <div className={classes.nav}>
+                <h1 className={classes.text}>Hola: {nombre}! </h1>
         </div>}
         </div>
     );
