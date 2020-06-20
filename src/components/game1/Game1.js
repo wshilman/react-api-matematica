f620159e57339d75cc65a9e87d8493a428b2e0ff
wshilman <walter.shilman@bue.edu.ar>
2020-06-20 13:03:29 -0300

    right arrow game 1

--- src/components/game1/Game1.js
+++ src/components/game1/Game1.js
@@ -6,7 +6,7 @@ import { useState } from 'react';
 import WinPage from '../../utils/WinPage'
 import CardDeck from 'react-bootstrap/CardDeck'
 import Grid from '@material-ui/core/Grid';
-import { Redirect} from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 
 import iconNext from '../../media/next.png';
 import iconPrev from '../../media/prev.png';
@@ -85,7 +85,9 @@ const Game1 = () => {
             </CardDeck>
             <Grid container spacing={3} style={{paddingTop:"20px"}}>
                 <Grid item xs={6}>
-                <img src={iconPrev} style={{width:"80px",cursor:"pointer"}} alt="Previo"/>
+                    <Link to='/niveles'>
+                        <img src={iconPrev} style={{width:"80px",cursor:"pointer"}} alt="Previo"/>
+                    </Link>
                 </Grid>
                 <Grid item xs={6}>
                 <img src={iconNext} style={{width:"80px",cursor:"pointer"}} alt="Proximo"/>
