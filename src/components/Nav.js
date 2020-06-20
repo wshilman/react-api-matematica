a5edd7c82dc26d0082260162c0e05cde34205050
wshilman <walter.shilman@bue.edu.ar>
2020-06-20 12:22:08 -0300

    sign out in nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { makeStyles } from '@material-ui/core/styles';
+import { Button } from "@material-ui/core";
 
 const useStyles = makeStyles({
     text:{
@@ -10,23 +11,38 @@ const useStyles = makeStyles({
         marginBottom:-10,
     },
     nav:{
-        height: "auto",
+        height: "60px",
         minHeight:"40px",
         backgroundColor:"#ffc10778",
         color:"#FF5722",
         padding:"20px"
+    },
+    divs:{
+        float:'left',
+        width:'34%'
     }
 });
 
 const Nav = ()=>{
     const classes = useStyles();
     const nombre = localStorage.getItem('nombre');
+    const handleClick = () => {
+        localStorage.removeItem('nombre');
+        window.location.reload();  
+    }
+    
 
     return(
         <div>
             {nombre &&
             <div className={classes.nav}>
-                    <h1 className={classes.text}>Hola: {nombre}! </h1>
+                <div className={classes.divs}>
+                    <h1 className={classes.text} >Hola: {nombre}! </h1>
+
+                </div>
+                <div className={classes.divs}>
+                    <Button className={classes.button} onClick={()=>{handleClick()}}> Cerrar Sesion</Button>
+                </div>
             </div>}
         </div>
     );
